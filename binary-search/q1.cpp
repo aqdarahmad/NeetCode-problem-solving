3e644dd5ddc7ca7b21cf77fcbf726c64b7990f80
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-01-18 11:19:00 +0200

    find targrt elemnt using binary search

---
+++ binary-search/q1.cpp
@@ -0,0 +1,25 @@
+//binary search
+//time complexity: O(log n)
+//space complexity: O(1)
+//problems 
+
+int binarysearch(const vector<int>&arr , int target)
+{
+    int left = 0;
+    int right = arr.size() -1;
+    while(left <= right)
+    {
+        int mid = left + (right - left) /2;
+        if(arr[mid] === target)
+        {
+            return mid;
+        }
+        else if(arr[mid]<target)
+        {
+            left = mid + 1;
+        }
+        else 
+        right = mid -1;
+    }
+    return -1;
+}
