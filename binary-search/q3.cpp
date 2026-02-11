6e6bbe3ebf0f6a43da1c7412beffcdd98f636edb
aqdarahmad <a.abusufyan@student.aaup.edu>
2026-02-11 18:52:23 +0200

    Binary search (#50)
    * solve contain dup problem
    
    * solve valid anagram
    
    * solve two sum pronlem brute force
    
    * group anagrams problem
    
    * solve group anagrams problem
    
    * top k freq  problem
    
    * solve palindrom problem
    
    * o(n/2)
    
    * reverse queue elements
    
    * sort a queue elements
    
    * sort a queue elements
    
    * sort a queue elements
    
    * add new items to linked list insert + display
    
    * reverse a linked list
    
    * COUNT NODES
    
    * insert at the begin
    
    * insert at the end
    
    * find mid element in linkedlist
    
    * find targrt elemnt using binary search
    
    * using binary search searching for a terget in matrix
    
    * using binary search searching for a terget in matrix
    
    * solve worms ques

---
+++ binary-search/q3.cpp
@@ -0,0 +1,40 @@
+/*
+
+5
+2 7 3 4 9
+[1,2]F
+*/
+// minimum element greate than or equal target = binary search
+
+
+int mybs(int tar , vector<int>v)
+{
+int l =0, r= v.size()-1;
+while(l<=r)
+{
+    int mid = l +(r-1)/2;
+    if(v[mid] >= tar)
+    r=mid-1;
+    else
+    l=mid+1;
+}
+return l;
+}
+
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int &i:v) cin>>i;
+    for(int i=0;i<n;i++)
+   v[i]+= v[i-1];
+   int q;cin>>q;
+    while(q--)
+    {
+         int tar;cin>>tar;
+         cout<<mybs(tar,v)+1<<endl;
+    }
+}
+
